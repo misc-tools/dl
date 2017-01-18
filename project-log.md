0a825f8b1cd0da8b45b8f097b81275b0ff79f2e1
Viet H Nguyen <viet.nguyen182@gmail.com>
2017-01-18 06:23:28 -0500

    list all links available on the website

--- project-log.md
+++ project-log.md
@@ -1,4 +1,4 @@
-## January 17, 2017 
+## Jan 17, 2017 
 
 - Start the project. 
 - Some libraries that I think should be used in this project: 
@@ -6,4 +6,15 @@
   + `optparse-applicative`: to parse command line argument 
   + `async`: for concurrent downloads. 
   
--   
+## Jan 18, 2017 
+
+- To implement: Given a URL, count how many resources of each type. 
+- Syntax should be: 
+```sh
+$ dl --explore --url www.google.com
+```
+
+- `optparse-applicative` supports 4 kinds of options: regular options,
+  flags, arguments and commands. 
+  
+- 
--- src/Lib.hs
+++ src/Lib.hs
@@ -3,30 +3,66 @@ module Lib
     ) where
 
 import Options.Applicative
+import Network.HTTP
+import Text.HTML.TagSoup
+import Data.Maybe
+data Option = Option
+  { url :: String
+  , explore :: Bool }
 
-data Sample = Sample
-  { hello :: String
-  , quiet :: Bool }
-
-sample :: Parser Sample
-sample = Sample
+sample :: Parser Option
+sample = Option
      <$> strOption
-         ( long "hello"
-        <> metavar "TARGET"
-        <> help "Target for the greeting" )
+         ( long "url"
+        <> short 'u'
+        <> metavar "URL"
+        <> help "URL from which we want to download" )
      <*> switch
-         ( long "quiet"
-        <> help "Whether to be quiet" )
+         ( long "explore"
+        <> short 'e'
+        <> help "Whether to explore the URL and report the resources available" )
 
-greet :: Sample -> IO ()
-greet (Sample h False) = putStrLn $ "Hello, " ++ h
-greet _ = return ()
+process :: Option -> IO ()
+process (Option u False) = putStrLn $ "So, you want to download from " ++ u
+process (Option u True) = getStatistics u
 
+sampleUrl :: String
+sampleUrl = "http://stanford.edu/~pyzhang/publication.html"
+
+-- | count how many files of each type from a website 
+getStatistics :: String -> IO ()
+getStatistics url = do
+  src <- getResponseBody =<< simpleHTTP (getRequest url)
+  let tags = parseTags src
+  let a_tags = getATags tags
+  let links = map fromJust $ filter isJust $ map extractLink a_tags
+  putStrLn $ "Getting statistics from " ++ url ++ "..."
+  putStrLn $ "Display all links..."
+  mapM_ putStrLn links
+
+-- | get all tags <a ...>
+getATags :: [Tag String] -> [Tag String]
+getATags tags = filter f tags
+  where f (TagOpen "a" _) = True
+        f _ = False 
+
+-- | extract link from <a ...> tag -- heavily template matching :(
+extractLink :: Tag String -> Maybe String
+extractLink (TagOpen "a" listparams) = go listparams
+  where go lps = case lps of
+          [] -> Nothing
+          p : ps -> case p of
+            ("href", link) -> Just link
+            _ -> go ps          
+extractLink _ = Nothing
+
+
+            
 someFunc :: IO ()
-someFunc = execParser opts >>= greet
+someFunc = execParser opts >>= process
   where
     opts = info (helper <*> sample)
       ( fullDesc
-     <> progDesc "Print a greeting for TARGET"
-     <> header "hello - a test for optparse-applicative" )
+     <> progDesc "Download resources from a website"
+     <> header "dl - Download resources from a website" )
          
